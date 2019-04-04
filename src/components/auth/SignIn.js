8a8b027ee0537f44c77fce70b12bb654e1038417
giovaniZanetti <zanetti.giovani@gmail.com>
2019-04-04 20:50:18 +0200

    create signin form

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,31 +2,33 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class SignIn extends Component {
-  
+
+  onSubmit = formValues => {
+    this.props.onSubmit(formValues)
+  }
+
   render() { 
+  console.log(this.props)
+
     return (
       <div className='container'>
-        <form className='white' onSubmit={this.handleSubmit}>
+        <form className='white' onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <h5 className='grey-text text-darken-3'>Sign In</h5>
           <div className='input-field'>
-            <label htmlFor='email'>enter your e-mail</label>
             <Field 
               name="email"
               component="input"
               type="email"
               placeholder="enter your email address"
-              id="email"
+              
             />
           </div>
           <div className='input-field'>
-            <label htmlFor='password'>Enter your password</label>
-            <input type='password' id='password' onChange={this.handleChange} />
             <Field 
               name="password"
               component="input"
               type="password"
               placeholder="enter your password"
-              id="password"
             />
           </div>
           <div className='input-field'>
