91a17c4a180f8cc432b6e17ccfac8b9a0ec01bea
giovaniZanetti <zanetti.giovani@gmail.com>
2019-04-12 19:06:11 +0200

    render RecipeList

--- src/components/recipe/RecipeList.js
+++ src/components/recipe/RecipeList.js
@@ -1,33 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { fetchRecipes } from '../../actions'
-import axios from 'axios'
 
 class RecipeList extends Component {
-  render() { 
-    return (
-      <div>
-        RecipeList
-      </div>
-    )
-  }
-
   componentDidMount = () => {
-    axios.get('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?', {
-      headers: {
-        "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-        "X-RapidAPI-Key": "8e1196b484mshaee2f822d87a11dp10a1dejsn3533f0b051b3"
-      },
-      params: {
-        number: 20
-      }
-    })
-    .then(data => console.log(data, 'I am the being fetched from componentDidMount'))
-    .then(error => console.log(error))
-
     this.props.fetchRecipes()
   }
+
+  recipeCardsRender = () => {
+    const { recipes } = this.props.recipes.recipes
+    return this.props.recipes.isPending
+      ? (
+          <div className="preloader-wrapper big active">
+            <div className="spinner-layer spinner-blue-only">
+              <div className="circle-clipper left">
+                <div className="circle"></div>
+              </div>
+              <div className="gap-patch">
+                <div className="circle"></div>
+              </div>
+              <div className="circle-clipper right">
+                <div className="circle"></div>
+              </div>
+            </div>
+          </div>
+        )
+      : recipes.map(recipe => (
+        <div className="card col s12 m6 l3" style={{padding: 5}} key={recipe.id}>
+          <div className="card-image waves-effect waves-block waves-light">
+            <img className="activator" src={recipe.image} alt="recipe" />
+          </div>
+          <div className="card-content">
+            <span className="card-title activator grey-text text-darken-4">{recipe.title}<i className="material-icons right">FLip the Card</i></span>
+            <p><a href="#">See the full recipe</a></p>
+          </div>
+          <div className="card-reveal">
+            <span className="card-title grey-text text-darken-4">{recipe.title}<i className="material-icons right">close</i></span>
+            <span>{`Ready in ${recipe.readyInMinutes} minutes`}</span>
+            <p>{recipe.glutenFree ? 'Gluten Free' : ''}</p>
+            <p>{recipe.lowFodMap ? 'Low Fod Map' : ''}</p>
+            <p>{recipe.vegetarian ? 'Vegetarin' : ''}</p>
+            <p>{recipe.vegan ? 'Vegan' : ''}</p>
+            <p>{recipe.dairyFree ? 'Dairy Free' : ''}</p>
+
+          </div>
+        </div>
+      ))
+  }
   
+
+  render() {
+    console.log(this.props.recipes)
+    return (
+        <div className="row">
+            {this.recipeCardsRender()}
+          </div>
+    )
+  }
 }
 
 
