3c1af48fa80bdbc2c6864a5f1b30fb3d26a6ac8e
giovaniZanetti <zanetti.giovani@gmail.com>
2019-04-22 10:24:48 +0200

    create Recipe Detail Card

--- src/components/recipe/RecipeDetail.js
+++ src/components/recipe/RecipeDetail.js
@@ -2,31 +2,77 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class Recipedetail extends Component {
-  renderInstructions = () => {
-    return recipe.analyzedInstructions.steps.map(step => <li>{`${step.number} - ${step.step}`}</li>)
-  }
+  
+  // renderInstructions = () => {
+  //   const extractSteps = this.props.recipe.analyzedInstructions.map(instruction => instruction.steps)
+  //   return this.props.recipe.analyzedInstructions.map(instruction => {
+  //     return (
+  //       <div>{`${instruction.name} ${extractSteps.map(step => <li>{step.step}</li>)}`}</div>
+  //       )
+  //   })
+    
+  // }
 
   renderOccasions = () => {
-    return recipe.occasions.length 
-    ? recipe.occasions.map((occasion, index) => !index === -1 ? `${occasion},` :`${occasion}.`)
-    : ''
-}
-  
+    return( 
+      this.props.recipe.occasions.length > 0 &&
+      this.props.recipe.occasions.map(
+        (occasion, index) => !index === -1 
+        ? ` ${occasion},` 
+        :`${occasion}.`)
+    )
+  }
+
+  renderIngredients = () => {
+    return this.props.recipe.extendedIngredients.map(ingredient => {
+      return (
+        <div key={ingredient.id}>
+          <span>{ingredient.name}</span>
+          {`: ${ingredient.measures.metric.amount} ${ingredient.measures.metric.unitLong}`}
+        </div>
+      )
+    })
+  }
+
   render() { 
+    console.log(this.props)
     return (
       <div className='container'>
         <div className='card'>
-          <h2>Title</h2>
-          <p>This recipe is a perfect</p>
-          <p>This recipe is a perfect combination for {this.renderOccasions()}</p>
+          <h2>{this.props.recipe.title}</h2>
+          <img  src={this.props.recipe.image} alt='dish'/>
+          <p>{this.props.recipe.occasions.length > 0 ? `this a perfect combination for ${this.renderOccasions()}`: 'Sorry, no occasions sugestions fro this recipe!'}</p>
+          <h3>Ingredients</h3>
+          <div>{this.renderIngredients()}</div>
           <h3>Instructions</h3>
-          <ul>{this.renderInstructions}</ul>
-          <h3>{this.recipe.cookingMinutes > 0 ? this.recipe.cookingMinutes : ''}</h3>
-  
+          {this.props.recipe.instructions}
+          <h6>{this.props.recipe.cookingMinutes > 0 
+            ? `This recipe takes ${this.props.recipe.cookingMinutes} minutes for cooking`
+            : ''}</h6> 
+          <div>
+            <span>{`Ready in ${this.props.recipe.readyInMinutes} minutes`}</span>
+            <p>{`Servings: ${this.props.recipe.servings}`}</p>
+            <p>{this.props.recipe.glutenFree ? 'Gluten Free' : 'Contains gluten'}</p>
+            <p>{this.props.recipe.lowFodMap ? 'Low Fod Map' : ''}</p>
+            <p>{this.props.recipe.vegetarian ? 'Vegetarian' : ''}</p>
+            <p>{this.props.recipe.vegan ? 'Vegan' : ''}</p>
+            <p>{this.props.recipe.dairyFree ? 'Dairy Free' : 'Contains dairy'}</p>
+          </div>
         </div>
+        
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+  console.log(state, 'I am the state from DEtail')
+
+  return {
+    recipe: state.selectedRecipe,
+    recipes: state.recipes
+  }
+}
+
  
-export default Recipedetail;
+export default connect(mapStateToProps)(Recipedetail)
