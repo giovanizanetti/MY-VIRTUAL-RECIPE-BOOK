9415f8fd4829a13c459b7c4785ebdc2aa6084a9b
giovaniZanetti <zanetti.giovani@gmail.com>
2019-04-22 10:27:14 +0200

    Create SelectedRecipeReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { reducer as formReducer } from 'redux-form'
 import { combineReducers } from 'redux'
 import recipesReducer from './recipesReducer'
+import selectedRecipeReducer from './selectedRecipeReducer'
 
 export default combineReducers({
   form: formReducer,
-  recipes: recipesReducer
+  recipes: recipesReducer,
+  selectedRecipe: selectedRecipeReducer  
 })
