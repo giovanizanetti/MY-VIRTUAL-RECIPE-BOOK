c7cb0d17fe6db6720d5c57b004227ae471cd9b1d
Giovani Zanetti <zanetti.giovani@gmail.com>
2019-06-09 21:07:30 +0200

    Create INgredient component

---
+++ src/components/recipe/recipeDetail/Ingredients.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Ingredients = ({ingredients}) => {
+  const ingredientsList = ingredients && ingredients.map((ingredient, i) => {
+    const { amount, unitShort } = ingredient.measures.metric
+    return (
+      <div key={i}>
+        {`- ${Math.round(amount)} ${unitShort} of ${ingredient.name}`}
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h3>Ingredients</h3>
+      { ingredientsList }
+    </div>
+  )
+}
+
+export default Ingredients
