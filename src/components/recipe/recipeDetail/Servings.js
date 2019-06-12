7291ff48f47f05309264e8ea2ebee1f4695a1d98
Giovani Zanetti <zanetti.giovani@gmail.com>
2019-06-12 21:14:11 +0200

    bug fix

--- src/components/recipe/recipeDetail/Servings.js
+++ src/components/recipe/recipeDetail/Servings.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const Servings = ({servings}) => {
-  const servPeople =
-    servings &&  servings > 1
-    ? `This recipe serves ${servings} people`
-    : `This recipe serves ${servings} person`
-
-  return <div className='container'>{servPeople}</div>
+  const servPeople = () => {
+    if (servings &&  servings !== 0) {
+      return servings > 1
+      ? `This recipe serves ${servings} people`
+      : `This recipe serves ${servings} person`
+     }
+     return null
+  }
+  return <div className='container'>{ servPeople() }</div>
 }
 
 export default Servings
