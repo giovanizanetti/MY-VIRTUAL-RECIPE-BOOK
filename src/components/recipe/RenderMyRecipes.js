99a780c71f1b146428676fb9d3d183927ce5148b
Giovani Zanetti <zanetti.giovani@gmail.com>
2019-07-09 20:20:14 +0200

    Create no recipes message

--- src/components/recipe/RenderMyRecipes.js
+++ src/components/recipe/RenderMyRecipes.js
@@ -7,13 +7,27 @@ import LoaderProgressBar from '../LoaderProgressBar'
 
 const MyRecipes = (props) => {
   const { recipes } = props
+  const renderRecipeList = recipes < 1
+    ? <div
+      className='container'
+        style={{ display: 'flex',
+                flexDirection: 'column',
+              }}
+      >
+        <span>You have no recipes!</span>
+        <div
+          className='container center'
+          style={{display: 'flex', justifyContent: 'space-around'}}>
+          <button className='btn'>Create a recipe</button>
+          <button className='btn'>Search for recipes</button>
+        </div>
+
+      </div>
+    : <RecipeList recipes={recipes} />
   return (
     !recipes
     ? <LoaderProgressBar />
-    :
-      <RecipeList
-        recipes={recipes}
-      />
+    : renderRecipeList
   )
 }
 
