8634deae9f7bb9acd6bca086e0336fcdb85b3486
Giovani Zanetti <zanetti.giovani@gmail.com>
2019-08-11 15:27:58 +0200

    Typo fix

--- src/config/spooncular.js
+++ src/config/spooncular.js
@@ -6,4 +6,4 @@ export default axios.create({
     "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     "X-RAPIDAPI-KEY": "0f1f47b39bmsh0e4d2a04bd035cdp1121bejsnf58a226a5005",
   },
-});
+})
