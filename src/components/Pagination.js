2d9be33c0345bd7cede3a6dc4c698e9d0d3fe669
Giovani Zanetti <giovanizanetti@MacBook-Pro-van-Giovani.local>
2019-09-15 17:38:22 +0200

    Disable on Chevron click on the first and last pages

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,8 +4,6 @@ const Pagination = (props) => {
     const { totalRecipes, recipesPerPage, paginate, currentPage} = props
     const pageNumbers = []
     const calculatePages = Math.ceil(totalRecipes / recipesPerPage)
-    const nextPage = currentPage + 1
-    const previousPage = currentPage - 1
 
     //Add nessessary pages to the paginator
     for(let i = 1; i <= calculatePages; i++) {
@@ -14,13 +12,26 @@ const Pagination = (props) => {
 
     const rightChevronClassName = currentPage !== 1 ? "waves-effect" : "disabled"
     const LeftChevronClassName = currentPage !== pageNumbers[pageNumbers.length - 1] ? "waves-effect" : "disabled"
-    console.log(pageNumbers[pageNumbers.length - 1], currentPage)
+    console.log(pageNumbers.slice(-1).pop(), currentPage)
+
+    const handleNextPage = () => {
+        const nextPage = currentPage + 1
+        const lastPage = pageNumbers.slice(-1).pop()
+        currentPage !== lastPage 
+        && paginate(nextPage)
+    }
+
+    const handlePreviousPage = () => {
+        const previousPage = currentPage - 1
+        currentPage !== 1
+        && paginate(previousPage)
+    }
 
     return (
         //show pagination only if is nessessary
         totalRecipes > recipesPerPage &&
         <ul className="pagination">
-            <li onClick={() => paginate(previousPage)} className={ rightChevronClassName }><a href="#!"><i className="material-icons">chevron_left</i></a></li>
+            <li onClick={ handlePreviousPage } className={ rightChevronClassName }><a href="#!"><i className="material-icons">chevron_left</i></a></li>
             { 
                 pageNumbers.map(number => {
                 const className = number === currentPage ?`active` : `waves-effect`
@@ -31,7 +42,7 @@ const Pagination = (props) => {
                     )
                 })
             }
-            <li onClick={() => paginate(nextPage)}className={ LeftChevronClassName }><a href="#!"><i className="material-icons">chevron_right</i></a></li>
+            <li onClick={ handleNextPage }className={ LeftChevronClassName }><a href="#!"><i className="material-icons">chevron_right</i></a></li>
         </ul>
     )
 }
