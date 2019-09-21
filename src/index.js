89e778aed8c296ab695cba33972756094657ebe9
giovanizanetti <zanetti.giovani@gmail.com>
2019-09-22 00:04:19 +0200

    Persist API recipes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
-import { store } from './store'
+import { store, persistor } from './store'
 import "materialize-css/dist/css/materialize.min.css"
+import { PersistGate } from 'redux-persist/integration/react'
+import LoaderProgressBar from './components/LoaderProgressBar'
+
 
 store.firebaseAuthIsReady.then(() => {
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <PersistGate loading={ <div className='container'><LoaderProgressBar /></div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
   document.getElementById('root'))
 })
