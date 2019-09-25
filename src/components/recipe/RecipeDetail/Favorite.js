41cb3e1008fa0931f0dc11ff82f49ab730d5c5eb
giovanizanetti <zanetti.giovani@gmail.com>
2019-09-25 09:56:12 +0200

    Implement tool tip for favorite

--- src/components/recipe/RecipeDetail/Favorite.js
+++ src/components/recipe/RecipeDetail/Favorite.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from 'react'
+import ReactTooltip from 'react-tooltip'
 
 const Favorite = ({ style, favorite, handleFavoriteClick })  => {
+    const tooltipMessage = !favorite ? 'Add to Favotites' : 'Remove from Favorites'
     return (
-        <a onClick={ handleFavoriteClick } href="#!">
+        <>
+            <a data-tip={ tooltipMessage } onClick={ handleFavoriteClick } href="#!">
+            {/* <p >Tooltip</p> */}
             {
                 favorite 
                 ? <i 
@@ -12,8 +16,9 @@ const Favorite = ({ style, favorite, handleFavoriteClick })  => {
                     style={ style }
                     className="material-icons">favorite_border</i> 
             }
-        </a>
-
+            </a>
+            <ReactTooltip />
+        </>
     )
 }
 
