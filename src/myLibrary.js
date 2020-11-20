334fb710b64a47dd31455311ab78e8aec2817bc0
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-20 09:34:26 +0100

    Update coments.

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,28 +1,27 @@
-
-export const isNumber = elem => /^[0-9]*$/.test(elem)
+export const isNumber = (elem) => /^[0-9]*$/.test(elem)
 
 export const trimString = (string, length) => {
   return string && string.length > length
-    ?  string.substring(0, length -3) + "..."
+    ? string.substring(0, length - 3) + '...'
     : string
 }
 
 // This function capitalize all letters from all strings from an array
-export const upCaseArrStrings = (arr) => arr.map(word => word.toUpperCase())
+export const upCaseArrStrings = (arr) => arr.map((word) => word.toUpperCase())
 
 /* This function:
   - extract all strings from the array,
   - check how many items'
   - separete by commas/'and'
-  - Not pure, depending on upCaseStrings  */
+  */
 export const arrToStringPunctuation = (arr) => {
-  if(arr && arr.length >= 0) {
-    if(arr.length === 1) return upCaseArrStrings(arr)[0]
-    if(arr.length === 2) return upCaseArrStrings(arr).join(' and ')
-    //This statement separete all items by commas and add 'and' for the last index
-    if(arr.length > 2) {
+  if (arr && arr.length >= 0) {
+    if (arr.length === 1) return upCaseArrStrings(arr)[0]
+    if (arr.length === 2) return upCaseArrStrings(arr).join(' and ')
+    // This statement separete all items by commas and add 'and' for the last index
+    if (arr.length > 2) {
       const addEnd = ' and ' + upCaseArrStrings(arr).splice(-1)
-      const addComma =  upCaseArrStrings(arr).slice(0, -1).join(', ')
+      const addComma = upCaseArrStrings(arr).slice(0, -1).join(', ')
       return addComma + addEnd
     }
     return null
@@ -34,9 +33,9 @@ export const arrToStringPunctuation = (arr) => {
     - transforms minutes into hours,
     - check for the correct plural and singular */
 export const timeFormat = (minutes) => {
-  if(minutes > 59) {
+  if (minutes > 59) {
     const roundedHours = Math.floor(minutes / 60)
-    const hours =  roundedHours >= 2 ? `${roundedHours}h` : `${roundedHours}h`
+    const hours = roundedHours >= 2 ? `${roundedHours}h` : `${roundedHours}h`
     const min = minutes % 60 === 0 ? '' : ` and ${minutes % 60}min`
     return hours + min
   }
@@ -45,6 +44,5 @@ export const timeFormat = (minutes) => {
 
 export const getDate = () => {
   let fullDate = new Date()
-  return fullDate.setHours(0,0,0,0);
+  return fullDate.setHours(0, 0, 0, 0)
 }
-    
