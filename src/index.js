326cac0a46f26cfb2820d61bee2bec22ad1f8608
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-20 10:19:48 +0100

    ref: Reorder imports

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
-import { Provider } from 'react-redux'
-import { store, persistor } from './store'
-import "materialize-css/dist/css/materialize.min.css"
-import { PersistGate } from 'redux-persist/integration/react'
 import LoaderProgressBar from './components/LoaderProgressBar'
 
+import { Provider } from 'react-redux'
+import { store, persistor } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import './index.css'
+import 'materialize-css/dist/css/materialize.min.css'
 
 store.firebaseAuthIsReady.then(() => {
   ReactDOM.render(
     <Provider store={store}>
-      <PersistGate loading={ <div className='container'><LoaderProgressBar /></div>} persistor={persistor}>
+      <PersistGate
+        loading={
+          <div className='container'>
+            <LoaderProgressBar />
+          </div>
+        }
+        persistor={persistor}
+      >
         <App />
       </PersistGate>
     </Provider>,
-  document.getElementById('root'))
+    document.getElementById('root')
+  )
 })
-
