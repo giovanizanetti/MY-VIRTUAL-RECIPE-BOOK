6d8a20f3159de6410f531ed08123ce28b72d4864
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-20 10:21:09 +0100

    ref: Reorder imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import './App.css'
-import Navigation from './components/Navigation'
 import { connect } from 'react-redux'
+
 import Router from './Router'
-//#f7e59c
-//#f0ab0c
+import Navigation from './components/Navigation'
+
+import './App.css'
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Navigation />
         <div
-          className="container"
+          className='container'
           style={{
-            paddingTop: "5em",
+            paddingTop: '5em',
             marginBottom: '10%',
-            height: '100vh'
-            }}>
+            height: '100vh',
+          }}
+        >
           <Router />
         </div>
       </BrowserRouter>
@@ -26,7 +28,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-return {state}
+  return { state }
 }
 
 export default connect(mapStateToProps)(App)
