6f9c72782f97f4616f0a7d16b38ed19c62a6224e
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-20 10:55:37 +0100

    Restore config folder.

--- src/components/recipe/RenderMyRecipes.js
+++ src/components/recipe/RenderMyRecipes.js
@@ -3,6 +3,7 @@ import { compose } from 'redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { connect } from 'react-redux'
 import { setSearchField } from '../../actions/searchActions'
+
 import RecipeList from './RecipeList'
 import LoaderProgressBar from '../LoaderProgressBar'
 import NoRecipes from './NoRecipes'
@@ -16,56 +17,61 @@ class MyRecipes extends Component {
 
   render() {
     const { recipes, searchField, setSearchField, match } = this.props
-    const filteredRecipes = recipes && recipes.filter(recipe => {
-      return recipe.title && recipe.title.toLowerCase().includes(searchField.toLowerCase())
-    })
-    const isFavorites = match.path === "/myRecipes/favorites"
-    
-    const favorites = filteredRecipes && filteredRecipes.filter(recipe => recipe.favorite)
+    const filteredRecipes =
+      recipes &&
+      recipes.filter((recipe) => {
+        return (
+          recipe.title &&
+          recipe.title.toLowerCase().includes(searchField.toLowerCase())
+        )
+      })
+    const isFavorites = match.path === '/myRecipes/favorites'
+
+    const favorites =
+      filteredRecipes && filteredRecipes.filter((recipe) => recipe.favorite)
     const handleSearch = (e) => setSearchField(e.target.value)
 
     const renderRecipeList =
-      recipes < 1
-      ? <NoRecipes />
-      : <>
-          <MyRecipesSearchBar handleSearch={ handleSearch } />
+      recipes < 1 ? (
+        <NoRecipes />
+      ) : (
+        <>
+          <MyRecipesSearchBar handleSearch={handleSearch} />
           <h4
             style={{
-              textAlign: 'center', 
-              fontFamily: 'roboto', 
-              margin: '1rem', 
-              textDecoration: 'underline'
+              textAlign: 'center',
+              fontFamily: 'roboto',
+              margin: '1rem',
+              textDecoration: 'underline',
             }}
-            >{!isFavorites ? 'My Recipes' : 'Favorites'}
+          >
+            {!isFavorites ? 'My Recipes' : 'Favorites'}
           </h4>
-          {
-            filteredRecipes
-            && searchField.length > 0
-            && filteredRecipes.length < 1
-            ? <span className='red-text'>Sorry! NO RECIPES FOUND!!</span>
-            : <RecipeList   
-                recipes={ !isFavorites ? filteredRecipes : favorites }
-                id={'myRecipes'} 
-                isFavorites={ isFavorites }
-                />
-          }
+          {filteredRecipes &&
+          searchField.length > 0 &&
+          filteredRecipes.length < 1 ? (
+            <span className='red-text'>Sorry! NO RECIPES FOUND!!</span>
+          ) : (
+            <RecipeList
+              recipes={!isFavorites ? filteredRecipes : favorites}
+              id={'myRecipes'}
+              isFavorites={isFavorites}
+            />
+          )}
         </>
+      )
 
-    return (
-      !recipes
-      ? <LoaderProgressBar />
-      : renderRecipeList
-    )
+    return !recipes ? <LoaderProgressBar /> : renderRecipeList
   }
 }
 
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const { firebase, firestore, selectedRecipe, search } = state
   const currentUser = firebase.auth.uid
   return {
-    recipes: firestore.ordered.recipes 
-      && firestore.ordered.recipes.filter(rec => rec.userId === currentUser),
+    recipes:
+      firestore.ordered.recipes &&
+      firestore.ordered.recipes.filter((rec) => rec.userId === currentUser),
     selectedRecipe: selectedRecipe,
     searchField: search.searchField,
   }
@@ -73,7 +79,9 @@ const mapStateToProps = state => {
 
 export default compose(
   connect(mapStateToProps, { setSearchField }),
-  firestoreConnect([{
-    collection: 'recipes'
-  }])
+  firestoreConnect([
+    {
+      collection: 'recipes',
+    },
+  ])
 )(MyRecipes)
