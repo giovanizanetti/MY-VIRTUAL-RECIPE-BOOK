2461e367ef048f746ed8ae626a04cb3ec9a015ee
giovanizanetti <zanetti.giovani@gmail.com>
2020-11-20 10:26:46 +0100

    Hide config folder from git.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { connect } from 'react-redux'
 import Router from './Router'
 import Navigation from './components/Navigation'
 
-import './App.css'
-
 class App extends Component {
   render() {
     return (
